df8f3baee964ea6c9f155f25d84482aa9c661007
GitExl <GitExl@users.noreply.github.com>
2022-12-29 15:35:16 +0100

    Add arguments for extractor to allow extracting single entries.

--- indexer/src/extract.py
+++ indexer/src/extract.py
@@ -2,6 +2,7 @@ import cProfile
 import time
 from pathlib import Path
 from typing import List, Optional, Tuple
+from optparse import OptionParser
 
 from extractors.archiveextractor import ArchiveExtractor
 from extractors.archivelistextractor import ArchiveListExtractor
@@ -66,16 +67,18 @@ class Extract:
         for writer_class in WRITERS:
             self.writers.append(writer_class(logger, config, storage))
 
-    def process_file(self, path_local: Path) -> Tuple[Optional[ExtractedInfo], Optional[Entry]]:
+    def process_file(self, path_local: Path, force_update: bool) -> Tuple[Optional[ExtractedInfo], Optional[Entry]]:
         path_local_base = path_local.parents[0] / path_local.stem
         path_idgames = path_local.relative_to(self.config.get('paths.idgames'))
         path_idgames_base = path_idgames.parents[0] / path_idgames.stem
         filename_base = path_local.stem
 
+        self.logger.info('Processing {}...'.format(path_idgames))
         entry = self.storage.get_entry_by_path(path_idgames)
 
         # Bail if the file has not been updated since the last scan.
-        if entry is not None and entry.file_modified >= int(path_local.stat().st_mtime):
+        if not force_update and entry is not None and entry.file_modified >= int(path_local.stat().st_mtime):
+            self.logger.decision('Skipping {} because it is not updated.'.format(path_idgames))
             return None, None
 
         # Ignore some files we'd rather not analyse.
@@ -84,7 +87,6 @@ class Extract:
             self.logger.decision('Ignoring {} because: {}'.format(path_idgames, ignore_reason))
             return None, None
 
-        self.logger.info('Processing {}...'.format(path_idgames))
         info = ExtractedInfo(
             path_local,
             path_local_base,
@@ -114,17 +116,38 @@ class Extract:
 
 
 def run():
+    parser = OptionParser()
+    parser.add_option("--file", dest="filename",
+                      help="Extract info from a single file only.")
+    parser.add_option("--force", dest="force",
+                      action="store_true", default=False,
+                      help="Force updates of entries even if they have not changed.")
+    parser.add_option("--clean", dest="clean",
+                      action="store_true", default=False,
+                      help="Cleans up database after processing.")
+    parser.add_option("--verbosity", dest="verbosity",
+                      default=Logger.VERBOSITY_INFO, type='int',
+                      help="Set visible logging verbosity level. From 0 for least to 4 for most verbose.")
+
+    (options, args) = parser.parse_args()
+
     config = Config()
-    logger = Logger(config.get('paths.logs'), Logger.VERBOSITY_INFO)
+    logger = Logger(config.get('paths.logs'), options.verbosity)
+
+    logger.info('Setting up...')
     storage = DBStorage(config)
     extract = Extract(config, logger, storage)
 
     time_now = int(time.time())
 
-    idgames_local_root = Path(config.get('paths.idgames'))
-    paths_system = list(idgames_local_root.rglob('*.zip'))
+    if options.filename:
+        paths_system = [Path(options.filename)]
+    else:
+        idgames_local_root = Path(config.get('paths.idgames'))
+        paths_system = list(idgames_local_root.rglob('*.zip'))
+
     for path_system in paths_system:
-        info, entry = extract.process_file(path_system)
+        info, entry = extract.process_file(path_system, options.force)
         if info is None:
             continue
 
@@ -158,18 +181,24 @@ def run():
         storage.save_entry_music(entry, info.music)
         storage.commit()
 
-    logger.info('Removing dead entries...')
-    storage.remove_dead_entries(paths_system)
-    logger.info('Removing orphaned authors...')
-    storage.remove_orphan_authors()
-    logger.info('Removing orphaned levels...')
-    storage.remove_orphan_levels()
-    logger.info('Removing orphaned text files...')
-    storage.remove_orphan_textfiles()
-    logger.info('Removing orphaned images...')
-    storage.remove_orphan_images()
-    logger.info('Removing orphaned music...')
-    storage.remove_orphan_music()
+    if options.clean:
+
+        # Only remove dead entries if all entries were scanned.
+        if not options.filename:
+            logger.info('Removing dead entries...')
+            storage.remove_dead_entries(paths_system)
+
+        logger.info('Removing orphaned authors...')
+        storage.remove_orphan_authors()
+        logger.info('Removing orphaned levels...')
+        storage.remove_orphan_levels()
+        logger.info('Removing orphaned text files...')
+        storage.remove_orphan_textfiles()
+        logger.info('Removing orphaned images...')
+        storage.remove_orphan_images()
+        logger.info('Removing orphaned music...')
+        storage.remove_orphan_music()
+
     storage.commit()
 
     storage.close()
