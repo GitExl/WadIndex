cd3a2c24d68f09bbd0928fe5dc9c6239f10daebd
GitExl <GitExl@users.noreply.github.com>
2024-10-19 15:51:25 +0200

    Update Dockerfile sources and packages.

--- db/init-database.sql
+++ db/init-database.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: idgames
 -- ------------------------------------------------------
--- Server version 5.5.5-10.10.2-MariaDB-1:10.10.2+maria~ubu2204
+-- Server version	11.5.2-MariaDB-ubu2404
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `authors` (
   `alias` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `authors_UN` (`alias`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10467 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,13 +41,13 @@ CREATE TABLE IF NOT EXISTS `authors` (
 CREATE TABLE IF NOT EXISTS `directories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `parent_id` int(10) unsigned DEFAULT NULL,
-  `collection` varchar(15) NOT NULL,
-  `path` varchar(127) NOT NULL,
-  `name` varchar(31) NOT NULL,
+  `collection` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `path` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(31) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `directories_collection_IDX` (`collection`,`path`) USING BTREE,
   KEY `directories_parent_id_IDX` (`parent_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=294 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `directories` (
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE IF NOT EXISTS `entry` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `collection` varchar(15) NOT NULL,
+  `collection` varchar(7) NOT NULL,
   `path` varchar(127) NOT NULL,
   `directory_id` int(10) unsigned DEFAULT NULL,
   `file_modified` int(10) unsigned NOT NULL,
@@ -78,9 +78,7 @@ CREATE TABLE IF NOT EXISTS `entry` (
   `build_time` text DEFAULT NULL,
   `comments` text DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `entry_entry_created_IDX` (`entry_created`) USING BTREE,
   KEY `entry_entry_updated_IDX` (`entry_updated`) USING BTREE,
-  KEY `entry_file_modified_IDX` (`file_modified`) USING BTREE,
   KEY `entry_game_IDX` (`game`) USING BTREE,
   KEY `entry_engine_IDX` (`engine`) USING BTREE,
   KEY `entry_is_singleplayer_IDX` (`is_singleplayer`) USING BTREE,
@@ -88,10 +86,12 @@ CREATE TABLE IF NOT EXISTS `entry` (
   KEY `entry_is_deathmatch_IDX` (`is_deathmatch`) USING BTREE,
   KEY `entry_directory_id_IDX` (`directory_id`) USING BTREE,
   KEY `entry_collection_IDX` (`collection`,`path`) USING BTREE,
+  KEY `entry_entry_created_IDX` (`entry_created`) USING BTREE,
+  KEY `entry_file_modified_IDX` (`file_modified`) USING BTREE,
   FULLTEXT KEY `entry_path_ft_IDX` (`path`),
   FULLTEXT KEY `entry_title_ft_IDX` (`title`),
   FULLTEXT KEY `entry_description_ft_IDX` (`description`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20582 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,11 +116,16 @@ CREATE TABLE IF NOT EXISTS `entry_authors` (
 CREATE TABLE IF NOT EXISTS `entry_images` (
   `entry_id` int(10) unsigned NOT NULL,
   `name` varchar(27) NOT NULL,
+  `index` smallint(5) unsigned NOT NULL DEFAULT 0,
   `width` smallint(5) unsigned NOT NULL,
   `height` smallint(5) unsigned NOT NULL,
   `is_primary` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `aspect_ratio` double DEFAULT 1,
+  `random` int(10) unsigned NOT NULL,
   PRIMARY KEY (`entry_id`,`name`),
-  KEY `entry_images_is_primary_IDX` (`is_primary`) USING BTREE
+  KEY `entry_images_is_primary_IDX` (`is_primary`) USING BTREE,
+  KEY `entry_images_index_IDX` (`index`) USING BTREE,
+  KEY `entry_images_random_IDX` (`random`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -133,9 +138,9 @@ CREATE TABLE IF NOT EXISTS `entry_images` (
 CREATE TABLE IF NOT EXISTS `entry_music` (
   `entry_id` int(10) unsigned NOT NULL,
   `music_id` int(10) unsigned NOT NULL,
-  `name` varchar(63) NOT NULL,
+  `name` varchar(63) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`entry_id`,`music_id`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `map_authors` (
   `map_id` int(10) unsigned NOT NULL,
   `author_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`map_id`,`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,7 +198,7 @@ CREATE TABLE IF NOT EXISTS `maps` (
   `nodes_gl` tinyint(3) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `maps_entry_id_IDX` (`entry_id`,`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=59441 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,13 +209,13 @@ CREATE TABLE IF NOT EXISTS `maps` (
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE IF NOT EXISTS `music` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(8) NOT NULL,
+  `type` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `hash` binary(20) NOT NULL,
   `duration` int(10) unsigned DEFAULT NULL,
   `size` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `music_hash_IDX` (`hash`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26794 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,4 +231,4 @@ CREATE TABLE IF NOT EXISTS `music` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-20 18:13:52
+-- Dump completed on 2024-10-19 15:45:54
